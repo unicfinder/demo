cf1bf1f60bdf54e08fdb80a08c707526823cde83
unicfinder <143192590+unicfinder@users.noreply.github.com>
2024-08-04 18:19:19 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -15,20 +15,4 @@ public class Whitespaces {
           
     }
 }
-public class Whitespaces {
 
-    public static void main(String[] args) {
-        String sentence = "T    his is b  ett     er.";
-        System.out.println("Original sentence: " + sentence);
-
-        sentence = sentence.replaceAll("\\s", "");
-        System.out.println("After replacement: " + sentence);
-        public static void main(String[] args) {
-        String sentence = "T    his is b  ett     er.";
-        System.out.println("Original sentence: " + sentence);
-
-        sentence = sentence.replaceAll("\\s", "");
-        System.out.println("After replacement: " + sentence);
-          
-    }
-}
